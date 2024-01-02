103b5c5a77cd814608046648a6805f6c0ec948a3
Vaapee <68216347+Vaapee@users.noreply.github.com>
2024-01-02 17:28:40 -0300

    main.py

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import streamlit as st
 import requests
 
-response = requests.get('https://api.github.com', headers={'Accept': 'application/vnd.github+json'}, timeout=5, allow_redirects=False, stream=True, cert=None, auth=None, cookies=None, json=None, files=None, data=None, proxies=None,
-                         hooks=None, verify=True
+import requests
+
+response = requests.get('https://api.github.com', headers={'Accept': 'application/vnd.github+json'}, timeout=5, allow_redirects=False, verify=True, stream=True, auth=None, cookies=None, json=None, files=None, proxies=None,
+                         hooks=None, stream=False, cert=None, data=None)
+
 print(response.text)
 
 def link_button(title, url):
